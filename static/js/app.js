8561ba091a16fd721546f6539edf9042027537f8
Data Boot Camp <chenylk@outlook.com>
2020-09-24 16:23:59 -0400

    updated code utilizing the function created

--- static/js/app.js
+++ static/js/app.js
@@ -4,9 +4,17 @@ const tableData = data;
 var tbody = d3.select('tbody');
 
 function buildTable(data) {
+    // First, clear out any existing data
     tbody.html('');
+
+    //Next, loop through each object in the data
+    // ana append a row and cells for each value in the row
     data.forEach((dataRow) => {
+        //Append a row to the table body
         let row = tbody.append('tr');
+
+        //loop through each field in the dataRow and add
+        //each value as a table cell (td)
         Object.values(dataRow).forEach((val) => {
             let cell = row.append('td');
             cell.text(val);
@@ -14,3 +22,29 @@ function buildTable(data) {
         );
     });
 }
+
+function handleClick() {
+    //Grab the datetime value from the filter
+    let date = d3.select('#datetime').property('value');
+    let filteredData = tableData;
+
+    //check to see if a date was enterd and filter the
+    //data using that date
+    if (date) {
+        //apply 'filter' to the table data to only keep the
+        //rows where the 'datetime' value matched the filter value
+        filteredData = filteredData.filter(row => row.datetime ===date);
+    };
+
+    //Rebuild the tablle using the filtered data
+    //@NOTE: If no date was entered, then filteredData will
+    //just be the original tableData
+    buildTable(filteredData);
+
+};
+
+//Attach an event to listen for the form buttom
+d3.selectAll('#filter-btn').on('click',handleClick);
+
+//Build the table when the page loads
+buildTable(tableData);
